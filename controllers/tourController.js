fb57f2c1f835f1312ff223d19bc46baf70b31bdf
Vinson Xavier <vinsonxavier12@gmail.com>
2023-08-18 09:43:41 +0530

    first commit

---
+++ controllers/tourController.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkId = (req, res, next, val) => {
+  console.log(`ID is ${val}`);
+  if (val * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'ID not found',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'failed',
+      message: 'invalid request',
+    });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const tour = tours.find((ele) => {
+    return ele.id == req.params.id;
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  let indexPos = undefined;
+  let tour = tours.find((ele, index) => {
+    if (ele.id == req.params.id) {
+      indexPos = index;
+      return true;
+    }
+  });
+  const updatedData = Object.assign(req.body, tour);
+  tours[indexPos] = updatedData;
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: updatedData,
+        },
+      });
+    }
+  );
+};
+
+exports.deleteTour = (req, res) => {
+  let indexPos = undefined;
+  let tour = tours.find((ele, index) => {
+    if (ele.id == req.params.id) {
+      indexPos = index;
+      return true;
+    }
+  });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
