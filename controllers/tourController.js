8accd04c11e6b4bc062b1ff4824ae18f45b1d8ae
Vinson Xavier <vinsonxavier12@gmail.com>
2023-08-21 21:05:24 +0530

    added for refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
 exports.checkId = (req, res, next, val) => {
   console.log(`ID is ${val}`);
   if (val * 1 > tours.length) {
@@ -29,41 +25,32 @@ exports.getAllTours = (req, res) => {
   res.json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // }, 
   });
 };
 
 exports.getTour = (req, res) => {
-  const tour = tours.find((ele) => {
-    return ele.id == req.params.id;
-  });
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((ele) => {
+  //   return ele.id == req.params.id;
+  // });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log('DB connected successfully!');
+  });
+
+// this is like the class and tourSchema is like the settings
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
